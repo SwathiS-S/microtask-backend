7e0acac518a33a1275b2d9a21a740156fc8035e9
Swathi S <swathis2205@gmail.com>
2026-04-25 11:25:24 +0530

    fix: add missing notification type enums

--- models/Notification.js
+++ models/Notification.js
@@ -26,9 +26,17 @@ const notificationSchema = new mongoose.Schema({
       'APPLICATION_REJECTED',
       'UPDATE_SUBMITTED',
       'WORK_SUBMITTED',
+      'WORK_APPROVED',
+      'WORK_REJECTED',
       'PAYMENT_RELEASED',
+      'PAYMENT_RECEIVED',
       'DISPUTE_RESOLVED',
-      'ESCROW_RELEASE_REQUEST'
+      'ESCROW_RELEASE_REQUEST',
+      'WITHDRAWAL_APPROVED',
+      'WITHDRAWAL_REJECTED',
+      'CHAT_MESSAGE',
+      'TASK_CANCELLED',
+      'TASK_COMPLETED'
     ],
     required: true
   },
@@ -43,6 +51,24 @@ const notificationSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
+  }
+});
+
+// Auto-trigger FCM Push Notification
+notificationSchema.post('save', async function (doc) {
+  try {
+    const User = mongoose.model('User');
+    const user = await User.findById(doc.recipient);
+    if (user && user.fcmToken) {
+      const { sendPushNotification } = require('../utils/firebase');
+      await sendPushNotification(user.fcmToken, doc.title, doc.message, {
+        type: doc.type,
+        notificationId: doc._id.toString(),
+        taskId: doc.taskId ? doc.taskId.toString() : ''
+      });
+    }
+  } catch (error) {
+    console.error('Error in notification post-save hook:', error);
   }
 });
 
